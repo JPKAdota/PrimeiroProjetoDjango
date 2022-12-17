6eaa1bc8f1c9aec5a3fc5da5ec4872c89cdcb15a
Kadota <joaokadota@gmail.com>
2022-12-16 22:52:43 -0300

    Criando Banco de dados com SQLite

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateField('date_published')
+
+    def __str__(self):
+        return self.question_text
+
+
+# Apagar perguntas e alternativas associadas a pergunta
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
